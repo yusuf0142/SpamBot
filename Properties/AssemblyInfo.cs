33fcd9f6d01b02287ac43019fa3e6f0ef35d873f
Yusuf Tuncay <noordlaann@gmail.com>
2023-01-22 15:05:47 +0100

    Changes to AssemblyInfo.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpamBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This program is a spam bot that allows users to insert text and adjust the speed and duration of the spam")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yusuf")]
 [assembly: AssemblyProduct("SpamBot")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("96663ce2-d403-4d6a-b758-2cc8a0b45d05")]
+[assembly: Guid("0b3348c5-b184-49a5-a5a5-f1528c1e729f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
